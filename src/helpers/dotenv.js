81822d8cc979ea3c56f6436b25e691310b00b7f0
Axel Navarro <navarroaxel@gmail.com>
2018-01-21 17:18:51 -0300

    feat(app): add validation for required AUTH_CLIENT_SECRET env var (#9)

--- src/helpers/dotenv.js
+++ src/helpers/dotenv.js
@@ -1,4 +1,11 @@
 require('dotenv').load();
+const winston = require('winston');
+
+if (!process.env.AUTH_CLIENT_SECRET) {
+    winston.error('AUTH_CLIENT_SECRET environment variable is required.');
+    process.exit(1);
+}
+
 process.env.NODE_ENV = process.env.NODE_ENV || 'development';
 process.env.PORT = process.env.PORT || 3000;
 process.env.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost';
