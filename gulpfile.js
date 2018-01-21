6a752a0e15daf709a4b93f1b9cab4a4a677844b6
Axel Navarro <navarroaxel@gmail.com>
2018-01-21 13:44:15 -0300

    feat(app): update node engine to v8.9 (#5)

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ const plugins = require('gulp-load-plugins');
 const $ = plugins();
 
 gulp.task('eslint', () =>
-    gulp.src(['**/*.js', '!node_modules/**', '!coverage/**'])
+    gulp.src(['**/*.js', '!node_modules/**', '!coverage/**', '!dist/**'])
         .pipe($.eslint())
         .pipe($.eslint.format())
         .pipe($.eslint.failAfterError())
@@ -21,45 +21,4 @@ gulp.task('mocha', () =>
         )
 );
 
-gulp.task('server', () => $.nodemon({
-    script: './src/www/index.js',
-    env: {NODE_ENV: process.env.NODE_ENV || 'development'},
-    ignore: ['./test/**/*.js'],
-    nodeArgs: ['--debug']
-}));
-
-gulp.task('clean:dist', () => require('del')('dist'));
-
-gulp.task('copy:dist', () =>
-    gulp
-        .src([
-            'index.js',
-            './config/**',
-            './src/**'
-        ], {base: '.'})
-        .pipe(gulp.dest('dist'))
-);
-
-gulp.task('package', () =>
-    gulp.src('./package.json')
-        .pipe($.jsonEditor(json => {
-            delete json.devDependencies;
-            return json;
-        }, {end_with_newline: true}))
-        .pipe(gulp.dest('dist/'))
-);
-
-gulp.task('dist', () =>
-    gulp.run(['copy:dist', 'package'])
-);
-
-gulp.task('build', ['clean:dist'], () =>
-    gulp.run(['dist'])
-);
-
-gulp.task('build:uat', ['clean:dist'], () =>
-    gulp.run(['dist'])
-);
-
 gulp.task('test', ['eslint']);
-gulp.task('dev', ['server']);
